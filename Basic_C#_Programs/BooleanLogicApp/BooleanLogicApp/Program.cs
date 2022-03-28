d348f09126cd2228e826d662fe29ab043000c340
TravLord <92835555+TravLord@users.noreply.github.com>
2022-03-28 12:33:02 -0600

    Insurance qualifier Console App

---
+++ Basic_C#_Programs/BooleanLogicApp/BooleanLogicApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Questions to verfiy user inputted info to see if they qualify for insurance
+            // Each questions input is converted to integers for proper boolean comparison
+
+            Console.WriteLine("Personal Insurance qualifier");
+            Console.WriteLine("What is your age?");
+            string ageQ = Console.ReadLine();
+            int ageQconv = Convert.ToInt32(ageQ);
+
+            Console.WriteLine("How many DUI's do you have?");
+            string duiQ = Console.ReadLine();
+            int duiQconv = Convert.ToInt32(duiQ);
+
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speedtikQ = Console.ReadLine();
+            int speedTikQconv = Convert.ToInt32(speedtikQ);
+
+            Console.WriteLine("Based off your answers, are you qualified? True or false?");
+
+            // Age must be greater than 15
+            bool qualifyAge = ageQconv > 15;
+
+            // Must have 0 DUI's
+            bool qualifyDuiQ = duiQconv < 1;
+
+            // Must have less than 4 speeding tickets
+            bool qualifySpeedTik = speedTikQconv < 4;
+
+            // if all conditions (above) are met the user qualifies for insurance 
+            Console.WriteLine(qualifyAge && qualifyDuiQ && qualifySpeedTik);
+            Console.ReadLine();
+
+
+        }
+    }
+}
