33708aec3fc9bbaa62d3664f1c47d57a1d0d605d
TravLord <92835555+TravLord@users.noreply.github.com>
2022-03-30 12:12:37 -0600

    Shipping Box Size Quote App.  Calculates quote based off of box dimensions and weight.

---
+++ Basic_C#_Programs/ShippingDimsAndQuoteApp/ShippingDimsAndQuoteApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingDimsAndQuoteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Package Express!  Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight?");
+            string weightQ = Console.ReadLine();
+            int weightConv = Convert.ToInt32(weightQ);
+
+            if (weightConv > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.  Have a good day.");
+            }
+            else
+                Console.WriteLine("What is the package width?");
+
+            string widthQ = Console.ReadLine();
+            int widthConv = Convert.ToInt32(widthQ);
+
+            Console.WriteLine("What is the package height?");
+            string heightQ = Console.ReadLine();
+            int heightConv = Convert.ToInt32(heightQ);
+
+            Console.WriteLine("What is the package length?");
+            string lengthQ = Console.ReadLine();
+            int lengthConv = Convert.ToInt32(lengthQ);
+
+            int totalDims = lengthConv + heightConv + widthConv;
+
+            decimal quoteDims = widthConv * heightConv * lengthConv;
+            decimal quoteDimsWeight = quoteDims * weightConv;
+            decimal quoteDimsTotal = quoteDimsWeight / 100.0000m;
+
+
+            if (totalDims > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            else
+                Console.WriteLine("Your estimated total for shipping this package is: " + "$" + quoteDimsTotal);
+
+            Console.ReadLine();
+        }
+    }
+}
