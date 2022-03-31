dc641f099b91a8343f40e75f004def46b439edf7
TravLord <92835555+TravLord@users.noreply.github.com>
2022-03-31 11:57:51 -0600

    While and Do while loop console app

---
+++ Basic_C#_Programs/ConsoleApp_DoWhileStmt/ConsoleApp_DoWhileStmt/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_DoWhileStmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // boolean while and do while loop console app
+            // Asks user a question and input starts while loop with corresponding response
+            Console.WriteLine("Wanna fight? please answer true or false.");
+            String CanUfight = Console.ReadLine();
+            bool CanFightConv = Convert.ToBoolean(CanUfight);
+
+            while (CanFightConv == true)
+            {
+                Console.WriteLine("Okay fisticuffs it is!");
+                break;
+            }
+            while (CanFightConv == false)
+            {
+                Console.WriteLine("Okay we'll hug instead...");
+                break;
+            }
+            
+
+            // asks user if they are human and prints response if answer is true
+            Console.WriteLine("Are you human? Please answer true or false.");
+            string Uname =Console.ReadLine();
+            bool UnameConv = Convert.ToBoolean(Uname);
+            if (UnameConv == true)
+            {
+
+
+                do
+                {
+                    Console.WriteLine("Hi there human!");
+                    break;
+                }
+                while (UnameConv == true);
+
+                {
+
+                }
+            }
+           
+          
+        }
+    }
+}
