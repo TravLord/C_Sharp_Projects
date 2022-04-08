25b06c9f31414e16dd43e61c4a26378556157225
TravLord <92835555+TravLord@users.noreply.github.com>
2022-04-07 22:48:30 -0600

    Console app class methods calls  from user input

---
+++ Basic_C#_Programs/ConsoleApp_CallingMethods/ConsoleApp_CallingMethods/MathOps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_CallingMethods
+{
+    public class MathOps
+    // Created class with 3 methods each containing math operations
+    // each called from main program when user gives input 
+    { 
+        public static int MultiplyUserEntry(int UserEntryNum)
+        {
+            int result = UserEntryNum * 95;
+            return result;
+        }
+
+        public static int DivideUserEntry(int UserEntryNum)
+        {
+            int result = UserEntryNum / 6;
+            return result;
+        }
+
+        public static int ModulusUserEntry(int UserEntryNum)
+        {
+            int result = UserEntryNum % 106;
+            return result;
+        }
+    }
+}
+
+    
+
---
+++ Basic_C#_Programs/ConsoleApp_CallingMethods/ConsoleApp_CallingMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_CallingMethods
+{
+     class Program
+    {
+        static void Main()
+        {
+            // Simple program that asks for a number and returns result to the console after
+            // some math operations are applied to it.
+
+            Console.WriteLine("What number do you want to do math operations on?");
+            string UserEntry = Console.ReadLine();
+            int UserEntryNum = Convert.ToInt32(UserEntry);
+
+            //Calling 3 class methods one by one and printing result of methods to console
+
+            MathOps.MultiplyUserEntry(UserEntryNum);
+            int resultMultiply = MathOps.MultiplyUserEntry(UserEntryNum);
+            Console.WriteLine("\nThis is the result of multiplying your entry by 95 --> " + resultMultiply);
+
+
+
+            MathOps.DivideUserEntry(UserEntryNum);
+            int resultDivide = MathOps.DivideUserEntry(UserEntryNum);
+            Console.WriteLine("\nThis is the result of dividing your entry by 6 --> " + resultDivide);
+
+
+            MathOps.ModulusUserEntry(UserEntryNum);
+            int resultModulus = MathOps.ModulusUserEntry(UserEntryNum);
+            Console.WriteLine("\nThis is the remainder of dividing your entry by 106 --> " + resultModulus);
+
+        }      
+    }
+}
