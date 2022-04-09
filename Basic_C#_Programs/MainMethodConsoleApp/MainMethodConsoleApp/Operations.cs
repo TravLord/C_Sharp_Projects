81e62879c11e524746176f374bba5a5b5429e9fa
TravLord <92835555+TravLord@users.noreply.github.com>
2022-04-08 21:20:46 -0600

    Main Method Console app - method returns of different data types with conversions.

---
+++ Basic_C#_Programs/MainMethodConsoleApp/MainMethodConsoleApp/Operations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodConsoleApp
+{
+    public class Operations      // This class contains 3 methods with identical
+                                 // name with 3 different overload parameters
+                                 // this allows many different data type
+                                 // inputs to be made and will
+                                 // still return a valid answer            
+    {
+        public Operations()
+        {
+
+        }     
+            //first addition method take in int and return answer as int
+            public static int Addition(int Num1)
+            {
+                int Sum = Num1 + 1134;
+                return Sum;
+            }
+        // second additon method with decimal parameter takes in decimal
+        // converts it to int after operation done
+        public static int Addition(decimal Num1decimal)
+        {
+           decimal SumDecimal = Num1decimal + 400.75m;
+            return Convert.ToInt32(SumDecimal);
+        }
+        // third addition method with string parameter, takes in string converts it to int
+        // then does math operation and returns int
+        public static int Addition(string NumString)
+        {           
+            int NumStringToInt = Convert.ToInt32(NumString);
+            int NumStringConv1Result = NumStringToInt / 6;           
+            return NumStringConv1Result;                     
+        }
+               
+    }
+    
+}
+
+
+
+
---
+++ Basic_C#_Programs/MainMethodConsoleApp/MainMethodConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodConsoleApp
+{
+    class Program
+    {                                   // object instantiation before each method call
+                                        // passing in int first decimal second and string third
+        static void Main()                  
+        {
+            Operations Ops = new Operations();
+            int Sum = Operations.Addition(875);
+
+            Console.WriteLine("The Addition method sum is: {0} ",Sum);
+            Console.ReadLine();
+
+            Operations DecimalOps = new Operations();
+            decimal Num1decimaltoInt = Operations.Addition(307.5m);
+
+            Console.WriteLine("The overloaded addition method sum is: {0} ", Num1decimaltoInt);
+            Console.ReadLine();
+
+            Operations StringNumOps = new Operations();
+            
+            int InputString = Operations.Addition("96");
+            Console.WriteLine("Here's that string back as an integer --> {0}", InputString);
+            Console.ReadLine();
+            
+        }        
+    }
+}
