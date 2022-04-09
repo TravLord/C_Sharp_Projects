65fed7fa04471d3215ce3d31fa22dba8fa10b16d
TravLord <92835555+TravLord@users.noreply.github.com>
2022-04-09 13:18:00 -0600

    Optional method paramters using user input. Takes in 1-2 parameter(s) and completes operation

---
+++ Basic_C#_Programs/ConsoleAppOptionalMethodParameters/ConsoleAppOptionalMethodParameters/OptionParams.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOptionalMethodParameters
+{
+    public class OptionParams
+    {
+        public static int twoInts(int firstUserEntryInt, int secondUserEntryInt = 1)
+        {
+            
+            int result = firstUserEntryInt + secondUserEntryInt;
+            return result;
+        }
+
+        
+
+    }
+}
---
+++ Basic_C#_Programs/ConsoleAppOptionalMethodParameters/ConsoleAppOptionalMethodParameters/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOptionalMethodParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This console app will take in user input and perform math operation.  user can give one value or two values
+            // and program will continue with default or user input depending on choice.
+
+            OptionParams OpParams = new OptionParams();
+            Console.WriteLine("Please enter a whole number for a math operation, then press enter. ");
+            int firstEntryInt = Convert.ToInt32(Console.ReadLine());
+
+            // second user entry tied to boolean to check if user input value if not input returns false
+            Console.WriteLine("Enter a second number if you would like, then press enter..  This is not required.");
+            var secondUserEntryIntIsValid = int.TryParse(Console.ReadLine(), out int secondEntryInt);
+
+            // declare result variable to return value back after conditional state determined
+            // if false boolean value only one parameter is given and default parameter value used
+            // if both values given operation with user value executed
+
+            int resultTwoInts;
+            if (secondUserEntryIntIsValid)
+            {
+                resultTwoInts = OptionParams.twoInts(firstEntryInt, secondEntryInt);
+            }
+            else
+            {
+                resultTwoInts = OptionParams.twoInts(firstEntryInt);
+            }
+           
+            Console.WriteLine("This is the result of a math operation --->{0}",resultTwoInts);
+            Console.ReadLine();
+        }
+    }
+}
