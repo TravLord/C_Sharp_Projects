f6e00212a632a8f9f5a4d2dee03ffa1bd765748e
TravLord <92835555+TravLord@users.noreply.github.com>
2022-04-10 19:08:18 -0600

    methods-objects utilizing class inheritance

---
+++ Basic_C#_Programs/Methods-Objects_ConsoleApp/Methods-Objects_ConsoleApp/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Objects_ConsoleApp
+{
+    public class Person
+    {
+        // two properties for name of person
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        // one method to print properties accessable from Employee class
+        public void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+}
---
+++ Basic_C#_Programs/Methods-Objects_ConsoleApp/Methods-Objects_ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Objects_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //employee initialization of object utilizing person classes method from inheritance
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+
+
+        }
+    }
+}
