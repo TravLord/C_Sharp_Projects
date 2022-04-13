ff4226e75d616e98fd9bf83c1cbb988b8c891da5
TravLord <92835555+TravLord@users.noreply.github.com>
2022-04-13 12:34:16 -0600

    Struct instantiation and assignment

---
+++ Basic_C#_Programs/StructConsoleApp/StructConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating struct object and assigning val then printing to console
+            Number number = new Number() { Amount = 45.5m };
+            Console.WriteLine(number.Amount);
+            
+
+          
+        }
+    }
+}
