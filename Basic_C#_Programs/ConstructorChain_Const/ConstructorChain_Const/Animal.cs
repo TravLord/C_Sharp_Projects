98ef6b2d2cdab7904216caa7c97ce9357939462c
TravLord <92835555+TravLord@users.noreply.github.com>
2022-04-19 12:55:28 -0600

    constructor chain with use of Const and var keywords

---
+++ Basic_C#_Programs/ConstructorChain_Const/ConstructorChain_Const/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChain_Const
+{
+    public class Animal
+    {
+        public Animal(string color):this(color, 4)
+        {
+
+        }
+        public Animal(string color, int legsCount)
+        {
+            Color = color;
+            LegsCount = legsCount;
+            Console.WriteLine("Garfield!!!");
+
+        }
+
+        public string Color { get; set; }
+        public int LegsCount { get; set; }
+
+
+
+    }
+}
---
+++ Basic_C#_Programs/ConstructorChain_Const/ConstructorChain_Const/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChain_Const
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string garfFaveFood = "Lasagna";
+            var Fur = "orange";            
+            Animal animal = new Animal(Fur);
+
+            Console.WriteLine("Garfield the cat has {0} fur and {1} legs, his favorite food is {2}.",Fur, animal.LegsCount, garfFaveFood);
+        }
+    }
+}
