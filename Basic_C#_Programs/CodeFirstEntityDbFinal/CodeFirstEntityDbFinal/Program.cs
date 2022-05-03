61f60765360f0e6619096486b6a8d274423f13d9
TravLord <92835555+TravLord@users.noreply.github.com>
2022-05-02 19:07:56 -0600

    Code First Entity Framework Student Database

--- Basic_C#_Programs/CodeFirstEntityDbFinal/CodeFirstEntityDbFinal/Program.cs
+++ Basic_C#_Programs/CodeFirstEntityDbFinal/CodeFirstEntityDbFinal/Program.cs
@@ -1,54 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace CodeFirstEntityDbFinal
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
             using (var context = new SchoolContext())
             {
                 var student = new Student() { Name = "Jabulba Triton" };
-
                 context.Students.Add(student);
                 context.SaveChanges();
                 Console.WriteLine(student);
             }
         }
-            
-    }
 
-    public class Student
-    {
-        public int StudentId { get; set; }
-        public string Name { get; set; }
-        public DateTime BirthDate { get; set; }
-        public Grade Grade { get; set; }
-        public int Age { get; set; }
-    }
 
-    public class Grade
-    {
-        public int IdGrade { get; set; }
-        public string ClassName { get; set; }
 
-        public string Quadrant { get; set; }
-        public ICollection<Student> Students { get; set; }
-    }
-
-    public class SchoolContext : DbContext // Deverived context-> this allows classes to be queired and have db structure
-    {
-        public SchoolContext(): base()
+        public class Student
         {
+            public int StudentId { get; set; }
+            public string Name { get; set; }
+            public virtual List<Grade> Grades { get; set; }
 
         }
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Grade> Grades { get; set; }
-    }
+        public class Grade
+        {
+            public int GradeId { get; set; }
+            public string ClassName { get; set; }
+            public virtual Student Student { get; set; }
+            public int StudentId { get; set; }
 
+        }
+
+        public class SchoolContext : DbContext // Deverived context-> this allows classes to be queired and have db structure
+        {
+            public DbSet<Student> Students { get; set; }
+            public DbSet<Grade> Grades { get; set; }
+        }
+    }
 }
